a11ca67583df204864cae88e927dc999af71719f
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-05-22 09:46:40 -0600

    Add files via upload

---
+++ adivinarnumv1.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	float num1;
+	float num2;
+	num1 = 2;
+	cout << "Ingresa un numero: " << endl;
+	cin >> num2;
+	if (num2==num1) {
+		cout << "Haz adivinado" << endl;
+	} else {
+		cout << "No haz adivinado :(" << endl;
+	}
+	return 0;
+}
+
