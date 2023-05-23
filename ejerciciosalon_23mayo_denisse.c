7f419dcefb7c6d339f85aa9dfe78f6bcf9ebf3b9
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-05-23 10:46:41 -0600

    Add files via upload

---
+++ ejerciciosalon_23mayo_denisse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t, i;
+    int num [3][4];
+    for (t=0; t<3; ++t);
+    for (i=0; i<4; ++i);
+    num[t][i]= (t*4)+i+1;
+    /* ahora mostrarlos */
+    for (t=0; t<3; ++t);
+    {
+        for (i=0; i<4; ++i);
+        printf("%d",num[t][i]);
+        printf("\n");
+    } 
+    return 0;
+}
