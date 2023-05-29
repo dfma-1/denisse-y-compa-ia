0324afd6d01996c47412577bedbccd910d851b44
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-05-29 08:50:58 -0600

    Add files via upload

---
+++ act.5b.29-05-23.c
@@ -0,0 +1,48 @@
+//programa numero
+
+//titulo: en el siguiente codigo Q es una variable global por estar definida fuera de las funciones y es accesible desde todas las sentencias, sin embargo las definiciones dentro de main, como A son locales a main
+//por consiguiente solo las sentencias anteriores a main pueden utilizar a
+
+
+//fecha:29/05/23
+
+
+//nombre: maria fernanda cisneros sanchez
+
+#include <stdio.h>
+
+int Q;
+int resultado1;
+int resultado2;
+
+int main()
+{
+    int A;
+    A = 124;
+    Q = 1;
+    resultado1 = A + Q;
+    printf("r1: %d\n", resultado1);
+
+    {
+        int B;
+        int resultado3;
+        B = 124;
+        A = 457;
+        Q = 2;
+        resultado2 = A + B + Q;
+        printf("r1a: %d\n", resultado1);
+        printf("r1b: %d\n", resultado2);
+    }
+
+    {
+        int C;
+        C = 124;
+        int B = 457;
+        A = 788;
+        Q = 3;
+        resultado2 = A + B + Q + C;
+        printf("r3: %d\n", resultado2);
+    }
+
+    return 0;
+}
