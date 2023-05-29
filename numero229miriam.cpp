846c13c078c3280a5b9a16f52d0bccf709f87c76
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-05-29 07:51:34 -0600

    Add files via upload

---
+++ numero229miriam.cpp
@@ -0,0 +1,21 @@
+/* programa numero 
+titulo
+fecha 
+nombre:Miriam Alvarez*/ 
+#include <stdio.h>
+
+ int main ()
+ 
+
+{
+	const int luna= 238857;
+	float luna_kilo; //distancia en millas 
+	printf("distancia a la luna %d millas\n",luna);
+	luna_kilo=luna*1.609;/*una milla es igual a 1.609 kilometros*/
+	printf("en kilometros es %.2fkm. \n", luna_kilo);
+	return 0
+	;
+}
+ 
+
+
