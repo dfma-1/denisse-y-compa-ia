81abe97ecf9b6dbadd0621f2c60e6a62eaa9e84f
AnielaHS <131935533+AnielaHS@users.noreply.github.com>
2023-05-29 10:57:34 -0600

    Add files via upload

---
+++ direccion.c
@@ -0,0 +1,4 @@
+printf("DAIRA ANIELA HERNANDEZ SILVA\n");
+printf("la cueva #29,Texoloc TLAXCALA\n");
+printf("LANDER EDUARDO MACUITL MORENO\n")
+printf("barrio del ranchito #31,Sta.Apolonia Teacalco TLAXCALA\n");
