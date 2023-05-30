ccc3cd77d1c24cc8e491370392367e2be17d073d
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-05-30 10:18:47 -0600

    Add files via upload

---
+++ ventas.mfcs.30-05.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+
+	int ventas;
+	int an;
+	int sal_min;
+	int IVA;
+	ventas=5000;
+	an=8;
+	sal_min=1000;
+	IVA=15;
+	if (ventas<sal_min*3&&an>>10*IVA);
+	
+	printf("resultado=%d");
+	return 0;
+}
