763d17dc4ef0dbcf9c6eb5c3c8bdf0f0348c9578
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-05-31 07:47:43 -0600

    Add files via upload

---
+++ act.mfcs.31-05.c
@@ -0,0 +1,14 @@
+//titulo:imprime el tamaño de valores de coma flotante y double
+//nombre:maria fernanda cisneros sanchez
+//fecha:31/05/23
+
+#include <stdio.h>
+int main ()
+
+{
+printf("el tamaño de variables de coma flotante es %d", sizeof (float));
+printf("bytes\n");
+printf("el tamaño de variables de doble precision es %d", sizeof (double));
+printf("bytes\n");
+return 0;
+}
