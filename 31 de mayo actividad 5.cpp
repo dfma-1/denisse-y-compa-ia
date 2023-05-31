3d24cc0db413b68a1cbc0fbe7ac799f63fe1739d
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-05-31 08:34:38 -0600

    Add files via upload

---
+++ 31 de mayo actividad 5.cpp
@@ -0,0 +1,16 @@
+/*programa numero 
+titulo:
+fecha :31 de mayo 
+nombre : miriam alvarez*/
+
+ #include <stdio.h>
+ int main ()
+ 
+ 
+ {
+ 	int  n=5, t;
+ 	t= ++n * --n;
+ 	printf("n= %d, t=%d\n",n,t);
+ 	printf("%d %d %d\n",++n, ++n, ++n);
+ 	return 0;
+ }
