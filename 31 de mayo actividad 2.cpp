692285997781a5b0c1ddec3cacacb3dac0374993
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-05-31 07:44:41 -0600

    Add files via upload

---
+++ 31 de mayo actividad 2.cpp
@@ -0,0 +1,17 @@
+/*programa numero 
+titulo:imprime el tamaño de  valores de coma flotante y doup 
+fechas:
+nombre: miriam amvales */
+
+#include<stdio.h>
+int main ()
+ 
+ {
+ 	printf("el tamaño de la varible de una coma flotante es %d", sizeof)float));
+ 	printf("bytes\n");
+ 	printf("el tamaño de la varible de doble orecision es %d",sizeof)double));
+ 	printf("bytes\n");
+ 	return 0;
+ 	
+ }
+
