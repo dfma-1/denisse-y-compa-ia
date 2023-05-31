feaf69b4a2a54f3f01f919f0d51ee4aa849f547d
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-05-31 08:35:22 -0600

    Add files via upload

---
+++ ejercicio31mayo_5.cpp
@@ -0,0 +1,15 @@
+/* nombre: denisse benitez galindo
+fecha:31 de mayo 2023
+titulo: operadores de incremento dos*/
+
+#include<stdio.h>
+
+int main()
+{
+ int n=5, t;
+ t= ++n * --n;
+ printf("n= %d, t=%d\n",n,t);
+ printf("%d %d %d\n",++n,++n,++n);
+ 
+ return 0;
+}
