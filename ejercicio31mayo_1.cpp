a3d6e89df3fa601d358597d60d4ee8034a088f09
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-05-31 07:46:45 -0600

    Add files via upload

---
+++ ejercicio31mayo_1.cpp
@@ -0,0 +1,16 @@
+/*Nombre: Denisse Benítez Galindo
+fecha:31 de mayo 2023
+titulo: imprime el tamaño de valores de coma flotante y double*/
+
+
+#include<stdio.h>
+
+int main()
+{
+printf("el tamaño de variables de coma flotante es %d", sizeof(float));
+printf("bytes\n");
+printf("el tamaño de varibales de doble precision es %d", sizeof(double));
+printf("bytes\n");
+
+return 0;
+}
