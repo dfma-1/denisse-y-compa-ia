d8f0207e02973cb84eace9cd5d78d8ce39cecfce
AnielaHS <131935533+AnielaHS@users.noreply.github.com>
2023-05-31 08:32:37 -0600

    Add files via upload

---
+++ 31.05.23_5.c
@@ -0,0 +1,14 @@
+/*programa numero
+titulo: ejercicio 5
+fecha:31,05,23
+nombre:DAIRA ANIELA HERNANDEZ SILVA*/
+#include<stdio.h>
+int main ()
+{
+  int n=5,t;
+  t= ++n * --n;
+  printf ("n= %d, t=%d\n",n,t);
+  printf("%d %d %d\n", ++n, ++n, ++n);
+  return 0;
+}
+  
