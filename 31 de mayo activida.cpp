ef5364e6d725188988299ab7367dfa6521a4b365
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-05-31 08:18:06 -0600

    Add files via upload

---
+++ 31 de mayo activida.cpp
@@ -0,0 +1,18 @@
+/* programa numero 
+titulo : test de operadores ++ y --
+fecha:31  de mayo 
+nombre:miriam alvarez*/
+
+#include<stdio.h>
+ int main ()
+ 
+ {
+ 	int m=45, n =75;
+ 	printf("m=%d, n=%d, \n",m,n);
+ 	++m;
+ 	n--;
+ 	printf("m=%d, n=%d\n",m,n);
+ 	m++;
+ 	m--;
+ 	printf("m=%d, n=%d, \n",m,n);
+}
