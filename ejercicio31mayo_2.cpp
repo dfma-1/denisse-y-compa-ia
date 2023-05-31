bb72ff8f5198372d32f88a4da85a6e523df3cfbd
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-05-31 07:56:25 -0600

    Add files via upload

---
+++ ejercicio31mayo_2.cpp
@@ -0,0 +1,27 @@
+/*denisse benitez galindo
+fecha: 31 de mayo 2023
+ejercicio: 2*/
+
+
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+int codigo, fahrenheit,coord_x,coord_y;
+
+codigo=3467;
+fahrenheit=123.456;
+coord_x=525;
+coord_y=725;
+a=b=c=45;
+a=(b=(c=45));
+
+printf("%i el valor de la variable codigo \n", codigo);
+printf("%i el valor de la variable fahrenheit \n", fahrenheit);
+printf("%i el valor de la variable coord_x \n", coord_x);
+printf("%i el valor de la variable coord_y \n", coord_y);
+printf("%i el valor de la variable a \n", a);
+printf("%i el valor de la variable b \n", b);
+printf("%i el valor de la variable c \n", c);
+}
