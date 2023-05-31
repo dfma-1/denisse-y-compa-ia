c6d9fc0fc8971425a678bc7b93f4eadeac62ad64
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-05-31 08:10:19 -0600

    Add files via upload

---
+++ ejercicio31mayo_3.cpp
@@ -0,0 +1,18 @@
+/* denisse benitez galindo
+fecha: 31 mayo 2023
+titulo: test de operadores ++ y --*/
+
+#include<stdio.h>
+
+int main()
+{
+int m=45, n=75;
+printf("m=%d, n=%d, \n ",m,n);
+++m;
+--n;
+printf("m=%d, n=%d\n",m,n);
+m++;
+n--;
+printf("m=%d, n=%d, \n",m,n);
+
+}
