e84883262c5ca224030c3c693bc2140dd0e31c09
AnielaHS <131935533+AnielaHS@users.noreply.github.com>
2023-05-31 08:20:17 -0600

    Add files via upload

---
+++ 31.05.23_3.c
@@ -0,0 +1,16 @@
+/*programa numero
+titulo:test de operadoresm ++ y --
+fecha:31,05,23
+nombre:DAIRA ANIELA HERNANDEZ SILVA*/
+#include<stdio.h>
+int main ()
+{
+  int m=45, n=75;
+  printf("m=%d, n=%d, \n" , m,n);
+  ++m;
+  --n;
+  printf("m=%d, n=%d\n" , m,n);
+  m++;
+  n--;
+  printf ("m=%d, n=%d, \n" , m,n);
+}
