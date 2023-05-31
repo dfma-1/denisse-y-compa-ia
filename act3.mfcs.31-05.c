27bcaa20083ede03696000686855ccdbdf07addf
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-05-31 08:13:24 -0600

    Add files via upload

---
+++ act3.mfcs.31-05.c
@@ -0,0 +1,17 @@
+//titulo:test de operadores ++ y --
+//nombre:maria fernanda cisneros sanchez
+//fecha:31/05/23
+
+#include <stdio.h>
+int main ()
+
+{
+int m=45, n=75;
+printf("m=%d, n=%d, \n",m,n);
+++m;
+--n;
+printf("m=%d, n=%d\n",m,n);
+m++;
+n--;
+printf("m=%d,n=%d,\n",m,n);
+}       
