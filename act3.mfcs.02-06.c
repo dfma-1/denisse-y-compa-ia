59dedaf22187d9ab7042130ed02778538aeec92f
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-06-02 09:57:53 -0600

    Add files via upload

---
+++ act3.mfcs.02-06.c
@@ -0,0 +1,30 @@
+// if else
+//mfcs
+//02-06-23
+
+#include <stdio.h>
+
+int main() {
+    int divisor, dividendo;
+    int cociente, residuo;
+    
+    printf("Ingrese el divisor: ");
+    scanf("%d", &divisor);
+    
+    printf("Ingrese el dividendo: ");
+    scanf("%d", &dividendo);
+    
+    if (dividendo != 0) {
+        if (divisor % dividendo == 0) {
+            cociente = divisor / dividendo;
+            printf("%d es divisible por %d. Cociente: %d\n", divisor, dividendo, cociente);
+        } else {
+            residuo = divisor % dividendo;
+            printf("%d no es divisible por %d. Residuo: %d\n", divisor, dividendo, residuo);
+        }
+    } else {
+        printf("Error: no se puede dividir entre cero.\n");
+    }
+    
+    return 0;
+}
