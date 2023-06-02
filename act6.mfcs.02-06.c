64b0c1d17fb8dec2577a63d1053e84e44aa4afab
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-06-02 10:53:21 -0600

    Add files via upload

---
+++ act6.mfcs.02-06.c
@@ -0,0 +1,24 @@
+//mfcs
+//02-06-23
+//mfcs. 02-06
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("El numero %d es positivo.\n", numero);
+    }
+    if (numero < 0) {
+        printf("El numero %d es negativo.\n", numero);
+    }
+    if (numero = 0) {
+        ("El numero %d es igual a cero.\n", numero);
+    }
+
+    return 0;
+}
