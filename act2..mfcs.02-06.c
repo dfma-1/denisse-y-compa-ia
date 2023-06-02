446696fa640c3a266790af122d6ae4fbe43aa36b
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-06-02 10:09:55 -0600

    Add files via upload

---
+++ act2..mfcs.02-06.c
@@ -0,0 +1,25 @@
+//act2.mfcs.02-06.c
+
+//mfcs
+//02-06-23
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Ingrese un número: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("El número es mayor que cero.\n");
+    }
+    if (numero < 0) {
+        printf("El número es menor que cero.\n");
+    }
+    if (numero = 0) {
+        ("El número es igual a cero.\n");
+    }
+
+    return 0;
+}
