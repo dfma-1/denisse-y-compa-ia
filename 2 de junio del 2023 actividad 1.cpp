29718af7e83232b459c13095585e51511a302882
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-02 10:12:37 -0600

    Add files via upload

---
+++ 2 de junio del 2023 actividad 1.cpp
@@ -0,0 +1,36 @@
+//desarrollar en leguaje c que solicite un calificacion obtenida entre 1 y 10 \\
+//y evelue si es mayor a 6 mande un mensaje de aprobado utilice la funcion cuts y printf  con decimales 
+  
+  #include<stdio.h>
+  
+  int main () {
+  	 float calificacion;
+  	 
+  	 printf("ingrese la calificacion:");
+  	 scanf("%f", &calificacion );
+  	 
+  	 if (calificacion > 6){
+  	 	puts ("aprobado");
+  	
+	}else{
+	  
+	    puts("reprobado");
+	      
+	      
+	}
+	return 0;  	 
+  	 	
+	}
+  
+  
+  	
+  
+  
+  
+
+  
+  	
+  	
+  
+  
+
