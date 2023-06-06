bd39b3a9a23b676ea414377c383b8780e977d7f4
mfer369 <132585249+mfer369@users.noreply.github.com>
2023-06-06 09:44:16 -0600

    Add files via upload

---
+++ act1.mfcs.06-06.c
@@ -0,0 +1,17 @@
+/*titulo: comportamiento o funcionamiento de una sentencia
+(bucle) while es:
+fecha: 06/06/23*/
+//mfcs
+#include <stdio.h>
+int main()
+{
+    int x=0;
+    
+    while (x<10)
+    printf("x:%d\n",x++);
+}    
+
+
+
+
+
