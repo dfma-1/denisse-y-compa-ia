71edd4c598965bb5a2dce95a9f7b7bbdc7ad42fc
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-06 10:41:29 -0600

    miri

---
+++ 6 de junio del 2023 actividad 3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+{
+	int contador =0;//inicializo la varible 
+	while (contador <5)//prueba logica 
+	
+	{
+		contador++; //si es verdadero realiza bloque de instrucciones 
+		printf("contador: %d\n",contador );
+		
+	}
+	
+	printf("terminado.contador:%d\n",contador);
+	return 0;
+}
