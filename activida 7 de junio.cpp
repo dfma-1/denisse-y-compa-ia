03d4d98b57898fc60af63562652b00644720a215
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-07 09:00:35 -0600

    Add files via upload

---
+++ activida 7 de junio.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main ()
+
+{
+	int digito_leido=0;//no se ha leido ningun numero 
+	char car;
+	while (!digito_leido)
+	
+	{
+		printf("introduzca un caracter:");
+		scanf("%c",&car);
+		digito_leido=((´0´<=car)) && ((´9´<=car));
+		
+	}
+	
+	return 0;
+	}
