945b93d20d9da9f15173f2213f379300c4cbb638
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-06 19:47:13 -0600

    miri

---
+++ 6 de junio del 2023 actividad 4.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+	int contador=1;
+	while(contador>100)//si la prueba logica es falsa ,no se ejecuta la sentencia de intrucci
+	
+	{
+		printf("%d\n",contador);
+		contador ++;
+		
+	}
+	return 0;
+}
