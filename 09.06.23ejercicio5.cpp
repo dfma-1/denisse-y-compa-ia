6103bf8e77119f0611158679e8ea22cb55313607
denisse-1209 <132584826+denisse-1209@users.noreply.github.com>
2023-06-09 10:52:07 -0600

    Add files via upload

---
+++ 09.06.23ejercicio5.cpp
@@ -0,0 +1,20 @@
+/*arregla este programa
+charchar=a''
+do
+printf(%D);car
+car++;
+while(<=z)*/
+
+#include <stdio.h>
+
+int main() {
+    char charchar =92;
+
+    do {
+        printf("%c\n", charchar);
+        charchar++;
+    } while (charchar <= 'z');
+
+    return 0;
+}
+
