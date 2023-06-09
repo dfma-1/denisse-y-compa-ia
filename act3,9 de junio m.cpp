c3b547a7c982481ac5c6de43c859836684d17220
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-09 10:39:57 -0600

    Add files via upload

---
+++ act3,9 de junio m.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    long int n, m, fact;
+	
+    do {
+        printf("\nIngrese el valor de n (entre 2 y 20): ");
+        scanf("%ld", &n);
+    } while (n < 2 || n > 20);
+
+    for (m = n, fact = 1; n > 1; fact *= n--) {}
+
+    printf("%ld! = %ld\n", m, fact);
+	
+    return 0;
+}
+
