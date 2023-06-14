73835dbcf30919b8fa9a50122e5f0871b76a0aa1
AnielaHS <131935533+AnielaHS@users.noreply.github.com>
2023-06-14 12:18:53 -0600

    Add files via upload

---
+++ 14_06_23 .4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{ 
+    obtenercaracteres();
+    albafetizar();
+    verletras();
+    return 0;
+}
+int obtenercaracteres()
+{
+    return 0;
+}
+int albafetizar()
+{
+    return 0;
+}
+void verletras()
+{ 
+    return ;
+}
