022352f23b322d53b214f87f4be2bfaef205bb25
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-14 12:51:13 -0600

    Add files via upload

---
+++ 14 dejunio act 5miri.cpp
@@ -0,0 +1,27 @@
+/*titulo:actividad 5 
+nombre:miriam alvarez 
+fecha:14 de junio del 2023*/
+
+
+
+#include<stdio.h>
+
+void func1(void){
+	
+	  puts("segunda funcion ");
+	 return;
+	}
+
+int func2(){
+     puts("tercera funcion ");
+      return 0;
+
+}
+
+int main() {
+	func1();
+	func2();
+	return 0;
+}
+
+
