f80967fa1aa9127c7a0b37ac265b74feee188a6c
AnielaHS <131935533+AnielaHS@users.noreply.github.com>
2023-06-14 12:44:23 -0600

    Add files via upload

---
+++ 14_06_23.7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int max (int x, int y)
+{
+    if (x<y)
+    return y;
+    else 
+    return x;
+}
+int main ()
+{
+    int m,n;
+    do {
+        printf("ingrese dos numeros ,de manera separada):");
+        scanf ("%d %d", &m,&n);
+        printf ("maximo de %d y %d es %d\n",m,n max(m,n));
+    } while (m !=0);
+    return 0;
+}
