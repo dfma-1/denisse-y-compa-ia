f9c3516b759672325765a31cb0084f2f3d5eaa9d
miriiam1 <132586852+miriiam1@users.noreply.github.com>
2023-06-20 17:50:36 -0600

    Add files via upload

---
+++ actv220dejunio.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+double cuadrado (double n);
+int main ()
+
+{
+	
+	double  x = 11.5;
+	printf ("%6.21f al cuadrado = %8.41f\n",x,cuadrado(x));
+	return 0;
+	
+}
+
+double cuadrado (double n )
+ 
+ {
+ 	return n*n;
+ }
